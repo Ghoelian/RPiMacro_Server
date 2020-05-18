d384206b181b934b69f151a95e8aaedc4ef9c76b
Julian Vos <post@julianvos.nl>
2020-05-18 09:31:17 +0200

    Converted to switch/case

--- index.js
+++ index.js
@@ -7,27 +7,35 @@ const port = 3000
 app.get('/macro', (req, res) => {
 	console.log('Request received with action ' + req.query.action)
 
-	if (req.query.action === 'obs') {
-		console.log('Muting OBS')
-		exec('"./mute_obs.ahk"')
-		res.sendStatus(200)
-	} else if (req.query.action === 'discord') {
-		console.log('Muting Discord')
-		exec('"./mute_discord.ahk"')
-		res.sendStatus(200)
-	} else if (req.query.action === 'toggleplay') {
-		console.log('Toggling play status')
-		exec('"./toggle_play.ahk"')
-		res.sendStatus(200)
-	} else if (req.query.action === 'previous') {
-		console.log('Previous track')
-		exec('"./previous_track.ahk"')
-		res.sendStatus(200)
-	} else if (req.query.action === 'next') {
-		console.log('Next track')
-		exec('"./next_track.ahk"')
-		res.sendStatus(200)
-	} else res.sendStatus(404)
+	switch (req.query.action) {
+		case 'obs':
+			console.log('Muting OBS')
+			exec('"./mute_obs.ahk"')
+			res.sendStatus(200)
+			break
+		case 'discord':
+			console.log('Muting Discord')
+			exec('"./mute_discord.ahk"')
+			res.sendStatus(200)
+			break
+		case 'toggleplay':
+			console.log('Toggling play status')
+			exec('"./toggle_play.ahk"')
+			res.sendStatus(200)
+			break
+		case 'previous':
+			console.log('Previous track')
+			exec('"./previous_track.ahk"')
+			res.sendStatus(200)
+			break
+		case 'next':
+			console.log('Next track')
+			exec('"./next_track.ahk"')
+			res.sendStatus(200)
+			break
+		default:
+			res.sendStatus(404)
+	}
 })
 
 app.listen(port)
